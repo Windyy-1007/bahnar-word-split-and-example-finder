01ff21049e75e261260369a47b6f1447d634cb60
Windyy-1007 <namphongtip02@gmail.com>
2024-10-21 23:44:44 +0700

    Update tool to combine sentences with same topic

--- sample_combine.py
+++ sample_combine.py
@@ -1,17 +1,21 @@
 import pandas as pd
 import xlsxwriter as xlsx
-import re
+
+inputfile = 'library/Từ điển mức câu_Gia Lai.csv'
+outputfileXLSX = 'output/GiaLai.xlsx'
+outputfileCSV = 'output/GiaLai.csv'
+
 
 def check_space(word):
     if word[0] == ' ':
         return word[1:]
     return word
 
-def dataFromXLSX(file):
+def dataFromCSV(file):
     data = [
         ['A', 'B', 'C'],
     ]
-    df1 = pd.read_excel(file)
+    df1 = pd.read_csv(file)
     # For each row, check 3 cells
     for row in range(df1.shape[0]):
         for col in range(df1.shape[1] - 2):
@@ -20,33 +24,35 @@ def dataFromXLSX(file):
             # Make sure cell 1, 2, 3 are strings
             cell1 = str(df1.iat[row, col])
             cell2 = str(df1.iat[row, col+1])
+            cell3 = str(df1.iat[row, col+2])
             # If cell2 is empty, skip to next row
-            if not cell2.strip() or not cell1.strip():
+            if not cell2.strip() or not cell1.strip() or not cell3.strip():
                 continue
             # Add row's content to data
-            if [cell2] == '':
-                continue
             cell1 = check_space(cell1)
             cell2 = check_space(cell2)
-            data.append([cell1, cell2])
+            cell3 = check_space(cell3)
+            data.append([cell1, cell2, cell3])
     return data
 
-def dataFromTexT(text):
-    # Make sure the text is a string
-    if not isinstance(text, str):
-        return
-    # From a textfile, split the text into a list of sentences (by . ? or !)
-    text = text.replace('\n', ' ')
-    sentences_list = re.split(r'[.!?]\s*', text)
-    
-    # Combine each 2 sentences into an element of a list
-    double_sentences_list = []
-    for i in range(0, len(sentences_list), 2):
-        if i + 1 < len(sentences_list):
-            double_sentences_list.append([sentences_list[i], sentences_list[i+1]])
-        else:
-            double_sentences_list.append([sentences_list[i], ''])
-    return double_sentences_list
+def combineSentences(data):
+    # For each row, combine cell1 with other cell1s, cell2 with other cell2s if their cell3s are the same
+    combined = []
+    DataSize = len(data)
+    for row in range(1, len(data)):
+        if(row % 200 == 0):
+            print('Row {} out of {}'.format(row, DataSize))
+        for row2 in range(row + 1, len(data)):
+            # Ensure both rows have at least 3 elements
+            if len(data[row]) >= 3 and len(data[row2]) >= 3:
+                if data[row][2] == data[row2][2]:
+                    # String in cell1 of row and row2 are combined into a new string and stored in cell1 of new row
+                    combined.append([data[row][0] + ' ' + data[row2][0], data[row][1] + ' ' + data[row2][1], data[row][2]])
+            else:
+                print('Row {} or {} has less than 3 elements'.format(row, row2))
+    combinedSize = len(combined)
+    print('Old data size: {}, New data size: {}'.format(DataSize, combinedSize))
+    return combined
 
 def dataToXLSX(data, outputfile):
     # Write data to output file
@@ -58,3 +64,19 @@ def dataToXLSX(data, outputfile):
             worksheet.write(row_num, col_num, cell_data)
     workbook.close()
     return
+
+def dataToCSV(data, outputfile):
+    # Write data to output file
+    df = pd.DataFrame(data)
+    df.to_csv(outputfile, index=False)
+    return
+
+
+def main():
+    data = dataFromCSV(inputfile)
+    combined = combineSentences(data)
+    dataToCSV(combined, outputfileCSV)
+    return
+
+if __name__ == '__main__':
+    main()
